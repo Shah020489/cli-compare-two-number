febfa7a7d6a33e808921f0e6a7f4975c7b04f71b
Shakeel Ahmed <shakeel_kashmiri@outlook.com>
2024-03-26 13:30:57 +0500

    first time files are commited.

---
+++ index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "num1",
+        message: "Enter the first number",
+        type: "number"
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter the second number"
+    }
+]);
+console.log("First", answer.num1);
+console.log("Second", answer.num2);
+if (answer.num1 > answer.num2) {
+    console.log("First is greater");
+}
+else {
+    console.log("Second is greater");
+}
